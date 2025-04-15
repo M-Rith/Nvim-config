57be8174c74e881c02bd141dc84547dd033379be
AppleD0g <user@Users-MacBook-Pro.local>
2025-04-16 03:11:54 +0700

    changing color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,27 +1,25 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      style = "storm", -- "storm", "night", "moon", "day"
-      transparent = false,
-      terminal_colors = true,
-      styles = {
-        comments = { italic = true },
-        keywords = { italic = true },
-        functions = {},
-        variables = {},
-        sidebars = "dark",
-        floats = "dark",
-      },
-      sidebars = { "qf", "help", "terminal", "lazy", "lspinfo", "trouble" },
-      dim_inactive = false,
-      lualine_bold = true,
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    style = "night", -- "storm", "night", "moon", "day"
+    transparent = true,
+    terminal_colors = true,
+    styles = {
+      comments = { italic = true },
+      keywords = { italic = true },
+      functions = {},
+      variables = {},
+      sidebars = "dark",
+      floats = "dark",
     },
-    config = function(_, opts)
-      require("tokyonight").setup(opts)
-      vim.cmd("colorscheme tokyonight")
-    end,
+    sidebars = { "qf", "help", "terminal", "lazy", "lspinfo", "trouble" },
+    dim_inactive = false,
+    lualine_bold = true,
   },
+  config = function(_, opts)
+    require("tokyonight").setup(opts)
+    vim.cmd("colorscheme tokyonight")
+  end,
 }
